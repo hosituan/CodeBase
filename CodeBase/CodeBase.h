cbda794d7cb4d38a168d6b8a80804647804d0027
HoSiTuan <tuan.ho@c2c-techhub.io>
2022-05-04 13:44:24 +0700

    Initial Commit

---
+++ CodeBase/CodeBase.h
@@ -0,0 +1,18 @@
+//
+//  CodeBase.h
+//  CodeBase
+//
+//  Created by Ho Si Tuan on 04/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CodeBase.
+FOUNDATION_EXPORT double CodeBaseVersionNumber;
+
+//! Project version string for CodeBase.
+FOUNDATION_EXPORT const unsigned char CodeBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodeBase/PublicHeader.h>
+
+
